9582a3b5b88069c1fe21d0b3821a8d275d4380d0
Icaro <ikaro_460@hotmail.com>
2023-08-29 19:04:54 -0300

    ex1 aula

--- listas_icaro/src/br/com/poo/aula/Exercicio1.java
+++ listas_icaro/src/br/com/poo/aula/Exercicio1.java
@@ -1,9 +1,27 @@
 package br.com.poo.aula;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 
 	public static void main(String[] args) {
+		String nome; 
+		String sobrenome;
 		
+		// instancia scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// entrada de dados
+		System.out.println("Qual seu nome? ");
+		nome = sc.nextLine();
+		System.out.println("E sobrenome? ");
+		sobrenome = sc.nextLine();
+		
+		// imprime o nome com sobrenome
+		System.out.println("Olá, "+nome+" "+sobrenome+", seja bem-vindo(a)!");
+		
+		//fecha scanner
+		sc.close();
 
 	}
 
