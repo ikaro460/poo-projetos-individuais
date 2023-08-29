a960ffacc7204561fc7b238db48fe15ffac9dfcb
Icaro <ikaro_460@hotmail.com>
2023-08-29 18:21:37 -0300

    create package monitoria

---
+++ listas_icaro/src/br/com/poo/monitoria/App.java
@@ -0,0 +1,12 @@
+package br.com.poo.monitoria;
+
+public class App {
+
+	public static void main(String[] args) {
+		ContaBancaria c1 = new ContaBancaria();
+		c1.agencia = 010;
+		c1.saldo = 1500;
+		c1.numero = 001;
+	}
+
+}
---
+++ listas_icaro/src/br/com/poo/monitoria/ContaBancaria.java
@@ -0,0 +1,17 @@
+package br.com.poo.monitoria;
+
+public class ContaBancaria {
+
+	int agencia;
+	double saldo;
+	int numero;
+
+	public ContaBancaria() {
+
+	}
+
+	public ContaBancaria(int ag, double sal, int num) {
+
+	}
+
+}
