7e7a0748ee3330ed20180b5291dd131398795803
Icaro <ikaro_460@hotmail.com>
2023-08-28 22:59:17 -0300

    lista 1 exercicio 4 completo

--- listas_icaro/src/br/com/poo/lista1/Exercicio4.java
+++ listas_icaro/src/br/com/poo/lista1/Exercicio4.java
@@ -1,10 +1,36 @@
 package br.com.poo.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//uma variavel pra cada unidade de medida de temperatura
+		double tempC, tempF;
+		
+		// instancia o scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//recebe o valor inserido pelo usuario
+		System.out.println("Digite a temperatura em Celsius: ");
+		
+		// leia tempC
+		tempC = sc.nextDouble();
+		limpa();
+		
+		
+		tempF = tempC * 1.8 + 32;
+		
+		System.out.println("Temperatura em Celsius: "+tempC+"\nTemperatura em Fahrenheit: "+tempF);
 
+	}
+	
+	// funcao limpa
+	public static void limpa() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println();
+		}
 	}
 
 }
