c55cc8999736292fdcb5013b6ac13f515d4749b8
Icaro <ikaro_460@hotmail.com>
2023-08-28 22:51:35 -0300

    lista 1 exercicio 2 completo

--- listas_icaro/src/br/com/poo/lista1/Exercicio2.java
+++ listas_icaro/src/br/com/poo/lista1/Exercicio2.java
@@ -1,10 +1,42 @@
 package br.com.poo.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// declaracao de variaveis
+		int arrayLength = 6;
+		int[] idades = new int[arrayLength]; 
+		int soma = 0;
+		
+		// instancia o scanner
+		Scanner sc = new Scanner(System.in);
+		
+		
+		for (int i = 0; i < arrayLength; i++) {
+			// Pergunta idade
+			System.out.println("Qual é a idade do colega "+(i+1)+"? ");
+			
+			// leia idade
+			idades[i] = sc.nextInt();
+			soma += idades[i];
+			limpa();
+		}
+
+		//fecha scanner
+		sc.close();
+		
+		System.out.println("O tempo de vida meu e dos meus colegas é "+soma+"!");
 
 	}
-	
+
+	// funcao limpa
+	public static void limpa() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println();
+		}
+	}
+
 }
