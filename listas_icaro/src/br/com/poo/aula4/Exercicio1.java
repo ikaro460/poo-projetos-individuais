15e24b44d0c168b8a4d075ddd715dde13352ecb6
Icaro <ikaro_460@hotmail.com>
2023-08-30 18:41:34 -0300

    ex1 e ex2 aula4

---
+++ listas_icaro/src/br/com/poo/aula4/Exercicio1.java
@@ -0,0 +1,23 @@
+package br.com.poo.aula4;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		int n1 = 25;
+		int n2 = 5;
+		int soma;
+		double divisao;
+		
+		soma = n1 + n2;
+		
+		divisao = (double)n1 / (double)n2;
+		
+		
+		
+		System.out.println("A soma é igual a: "+soma);
+		System.out.println("A divisão é igual a: "+divisao);
+		
+
+	}
+
+}
---
+++ listas_icaro/src/br/com/poo/aula4/Exercicio2.java
@@ -0,0 +1,17 @@
+package br.com.poo.aula4;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 'Z';
+
+		int sum = c1 + c2;
+		
+		System.out.println("Valor da soma: " + sum);
+		
+		// Na tabela ASCII o valor de 'A' é igual a 65 e o valor de 'Z' é igual a 90
+
+	}
+
+}
