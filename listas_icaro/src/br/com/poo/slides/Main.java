f4d2cc7a82c8214941e753958cd113565258c7f4
Icaro <ikaro_460@hotmail.com>
2023-08-31 19:14:05 -0300

    update main exemplos pro slide

--- listas_icaro/src/br/com/poo/slides/Main.java
+++ listas_icaro/src/br/com/poo/slides/Main.java
@@ -3,11 +3,17 @@ package br.com.poo.slides;
 public class Main {
 	public static void main(String[] args) {
 
+		System.out.println("Livro");
+		System.out.println();
+		
 		Livro l = new Livro("Aventuras Incríveis", "Autor Desconhecido", 200);
 		System.out.println(l.getTitulo());
 		System.out.println(l.getAutor());
 		System.out.println(l.getPaginas());
 		
+		System.out.println();
+		
+		System.out.println("LivroFiccao");
 		System.out.println();
 		
 		LivroFiccao lf = new LivroFiccao("Aventuras Científicas", "Autor Mais desconhecido", 200, "Ficção Científica");
@@ -19,7 +25,10 @@ public class Main {
 		
 		System.out.println();
 		
-		LivroNaoFiccao lnf = new LivroNaoFiccao("Aventuras Nao Científicas", "Autor Mais desconhecido", 200, "Ficção Científica");
+		System.out.println("LivroNaoFiccao");
+		System.out.println();
+		
+		LivroNaoFiccao lnf = new LivroNaoFiccao("Aventuras Nao Científicas", "Autor Mais desconhecido", 198, "Ficção Não Científica");
 		System.out.println(lnf.getTitulo());
 		System.out.println(lnf.getAutor());
 		System.out.println(lnf.getPaginas());
