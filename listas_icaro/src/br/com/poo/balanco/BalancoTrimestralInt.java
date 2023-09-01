bc66e200a4389103eb68ff568b560cf238124c3c
Icaro <ikaro_460@hotmail.com>
2023-09-01 00:28:34 -0300

    change int to integer

--- listas_icaro/src/br/com/poo/balanco/BalancoTrimestralInt.java
+++ listas_icaro/src/br/com/poo/balanco/BalancoTrimestralInt.java
@@ -7,11 +7,11 @@ public class BalancoTrimestralInt {
 	
 	private static final Logger logger = Logger.getLogger(BalancoTrimestralInt.class.getName());
 
-	private int jan = 15000;
-	private int fev = 23000;
-	private int mar = 17000;
+	private Integer jan = 15000;
+	private Integer fev = 23000;
+	private Integer mar = 17000;
 	
-	private int somaTotal = jan + fev + mar;
+	private Integer somaTotal = jan + fev + mar;
 	
 	public int soma() {
 		logger.log(Level.INFO, "O balanço trimestral é de R$: " + somaTotal);
@@ -29,7 +29,7 @@ public class BalancoTrimestralInt {
 		logger.log(Level.INFO, "O balanço trimestral é de R$: " + somaTotal);
 		return somaTotal;
 	}
-	
+
 	public int soma(int janeiro, int fevereiro, int marco) {
 		somaTotal = janeiro + fevereiro + marco;
 		logger.log(Level.INFO, "O balanço trimestral é de R$: " + somaTotal);
