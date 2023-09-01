738c985ad7c1a630cbd872dfc2274536d9de6819
Icaro <ikaro_460@hotmail.com>
2023-09-01 00:27:27 -0300

    create package contaBancaria

---
+++ listas_icaro/src/br/com/poo/contaBancaria/Conta.java
@@ -0,0 +1,10 @@
+package br.com.poo.contaBancaria;
+
+public class Conta {
+	
+	int numero;
+	String titular;
+	double saldo;
+	
+	
+}
---
+++ listas_icaro/src/br/com/poo/contaBancaria/SistemaInterno.java
@@ -0,0 +1,15 @@
+package br.com.poo.contaBancaria;
+
+public class SistemaInterno {
+
+	public static void main(String[] args) {
+		Conta minhaConta = new Conta();
+		minhaConta.titular = "Pedro";
+		minhaConta.saldo = 1000;
+		System.out.println(minhaConta);
+
+		
+		
+	}
+
+}
