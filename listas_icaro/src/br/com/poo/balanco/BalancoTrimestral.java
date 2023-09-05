0001715e23cf7d09fc735a687ebb969dba6bf837
Icaro <ikaro_460@hotmail.com>
2023-09-05 01:36:06 -0300

    feat: metodo operacoesBancarias

--- listas_icaro/src/br/com/poo/balanco/BalancoTrimestral.java
+++ listas_icaro/src/br/com/poo/balanco/BalancoTrimestral.java
@@ -1,29 +1,30 @@
 package br.com.poo.balanco;
 
 import java.math.BigDecimal;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class BalancoTrimestral {
 
 	public static void main(String[] args) {
 		
 		// instancia do valor int
-//		BalancoTrimestralInt bti = new BalancoTrimestralInt();
-//		bti.soma();
-//		bti.soma(100, 112389);
+		BalancoTrimestralInt bti = new BalancoTrimestralInt(1200);
+		bti.soma();
+		bti.soma(100, 112389);
 		
 		
 		// instancia do valor double
 		BalancoTrimestralDouble btd = new BalancoTrimestralDouble();
 		btd.soma();
-		//btd.soma(15000.50, 1500.99);
+		btd.soma(15000.50, 1500.99);
 		
 		
 		// instancia do valor BigDecimal
-//		BalancoTrimestralBigDecimal btg = new BalancoTrimestralBigDecimal();
-//		btg.soma();
-//		btg.soma(new BigDecimal(100));
+		BalancoTrimestralBigDecimal btg = new BalancoTrimestralBigDecimal();
+		btg.soma();
+		btg.soma(new BigDecimal(100));
+		btg.soma(new BigDecimal(100), new BigDecimal(4500));
+		btg.soma(new BigDecimal(100), new BigDecimal(4500), new BigDecimal(15000));
 	}
 
 }
